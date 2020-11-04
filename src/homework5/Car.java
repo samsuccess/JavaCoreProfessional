13b479b8878c41b313e52508096f9e7d258dbe7c
samsuccess <samsuccess36@gmail.com>
2020-11-04 17:47:23 +0300

    change homework5

--- src/homework5/Car.java
+++ src/homework5/Car.java
@@ -1,36 +1,69 @@
 package homework5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static boolean winner;
+    private static Lock win = new ReentrantLock();
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+    private int count;
+    private CyclicBarrier cb;
+    private CountDownLatch cdl;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public int getCount() {
+        return count;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdl) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
+        this.cdl = cdl;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cb.await();
+            cb.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWin(this);
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+    }
+
+    private static synchronized void checkWin(Car c) {
+        if (!winner) {
+            System.out.println(c.name + " - WIN");
+            winner = true;
         }
     }
 }
